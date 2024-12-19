54d46c4284996e62479d41f9fdd5c2b1362d4e54
Jose_Miguel_diaz <josem.diaz6620@gmail.com>
2024-12-19 12:58:04 +0100

    Refactorizar

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 import peliculas from './peliculas.js'
 
+const showFilms = (container, generoId) => {
+    const containerId = document.getElementById(container)
+    const generoFilter = peliculas.filter(peli => peli.genre_ids.includes(generoId))
+
+    for (let pelicula of generoFilter) {
+        const card = document.createElement('div')
+        card.className = 'card'
+        const img = document.createElement('img')
+        img.src = `https://image.tmdb.org/t/p/w200${pelicula.poster_path}`
+        img.alt = pelicula.original_title
+        const title = document.createElement('p')
+        title.innerText = pelicula.original_title
+
+        card.appendChild(img)
+        card.appendChild(title)
+        containerId.appendChild(card)
+    }
+}
+
+showFilms('genero-28', 28)
+showFilms('genero-53', 53)
+showFilms('genero-12', 12)
