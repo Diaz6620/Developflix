f4a38ef011562b5bf30506051e1c553ebb1c3403
Jose_Miguel_diaz <josem.diaz6620@gmail.com>
2024-12-19 23:11:11 +0100

    Finish

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
 import peliculas from './peliculas.js'
 
 const showFilms = (container, generoId) => {
-    const containerId = document.getElementById(container)
+    let containerId = document.getElementById(container)
     const generoFilter = peliculas.filter(peli => peli.genre_ids.includes(generoId))
 
-    for (let pelicula of generoFilter) {
-        const card = document.createElement('div')
-        card.className = 'card'
-        const img = document.createElement('img')
-        img.src = `https://image.tmdb.org/t/p/w200${pelicula.poster_path}`
-        img.alt = pelicula.original_title
-        const title = document.createElement('p')
-        title.innerText = pelicula.original_title
-
-        card.appendChild(img)
-        card.appendChild(title)
-        containerId.appendChild(card)
-    }
+    generoFilter.forEach(pelicula => {
+        const template = `
+            <div class='card'>
+                <img src="https://image.tmdb.org/t/p/w200${pelicula.poster_path}" alt="${pelicula.original_title}">
+                <p>${pelicula.original_title}</p>
+            </div>
+        `
+        containerId.innerHTML += template
+    })
 }
 
 showFilms('genero-28', 28)
